17ef91a4c40e2de9723d2c97a4fe62a43c0fe514
Csapii <mail.szasz.csaba@gmail.com>
2026-05-22 19:48:48 +0200

    2 diff printing style for better debugging and readability for LogEntry

--- apache_log_analyzer.py
+++ apache_log_analyzer.py
@@ -21,6 +21,25 @@ class LogEntry:
         if self.query_string != "":
             return self.uri + "?" + self.query_string
         return self.uri
+    
+    def __str__(self) -> str:
+        qs = f"?{self.query_string}" if self.query_string else ""
+        return (
+            f"[{self.timestamp}] "
+            f"{self.ip} "
+            f"{self.method} "
+            f"{self.uri}{qs} "
+            f"-> {self.status_code}"
+        )
+    def __repr__(self) -> str:
+        return (
+            f"LogEntry(ip={self.ip!r}, "
+            f"timestamp={self.timestamp!r}, "
+            f"method={self.method!r}, "
+            f"uri={self.uri!r}, "
+            f"query_string={self.query_string!r}, "
+            f"status_code={self.status_code!r})"
+        )
 
 class SignatureDB:
     """
