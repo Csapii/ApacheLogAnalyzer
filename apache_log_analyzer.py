b6fecaf77f10a5e785df2c90b70c83eed71e8094
Csapii <mail.szasz.csaba@gmail.com>
2026-05-22 17:05:15 +0200

    more readable and raw print formats for LogEntry

--- apache_log_analyzer.py
+++ apache_log_analyzer.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from urllib.parse import urlparse
 from dataclasses import dataclass
 
+
 @dataclass
 class LogEntry:
     ip: str
@@ -14,3 +15,23 @@ class LogEntry:
     uri: str
     query_string: str
     status_code: int
+
+    def __str__(self) -> str:
+        qs = f"?{self.query_string}" if self.query_string else ""
+        return (
+            f"[{self.timestamp}] "
+            f"{self.ip} "
+            f"{self.method} "
+            f"{self.uri}{qs} "
+            f"-> {self.status_code}"
+        )
+
+    def __repr__(self) -> str:
+        return (
+            f"LogEntry(ip={self.ip!r}, "
+            f"timestamp={self.timestamp!r}, "
+            f"method={self.method!r}, "
+            f"uri={self.uri!r}, "
+            f"query_string={self.query_string!r}, "
+            f"status_code={self.status_code!r})"
+        )
