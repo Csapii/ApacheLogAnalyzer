0472d69f9beabc40b9436af619b46feaba967608
Csapii <mail.szasz.csaba@gmail.com>
2026-05-22 15:19:42 +0200

    add LogEntry dataclass to standardize request logging

--- apache_log_analyzer.py
+++ apache_log_analyzer.py
@@ -5,3 +5,12 @@ import argparse
 from datetime import datetime
 from urllib.parse import urlparse
 from dataclasses import dataclass
+
+@dataclass
+class LogEntry:
+    ip: str
+    timestamp: datetime
+    method: str
+    uri: str
+    query_string: str
+    status_code: int
